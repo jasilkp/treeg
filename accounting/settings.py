f158cc438aaba9b0b9e644154bd0c5b7d8497d1a
Jasil kp <jasiljinu899@gmail.com>
2025-05-11 10:03:42 +0530

    Commit all changes

--- accounting/settings.py
+++ accounting/settings.py
@@ -103,11 +103,15 @@ WSGI_APPLICATION = 'accounting.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Acclive',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
+ 
 
 
 # Password validation
