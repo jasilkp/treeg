eb4e61fcb628266ed05c7f077daefbd24f25e189
Jasil kp <jasiljinu899@gmail.com>
2025-05-13 22:47:08 +0530

    Remove createsuperuser from build.sh to prevent duplicate user error on deployment

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py createsuperuser --noinput --username admin --email admin@example.com 
+# python manage.py createsuperuser --noinput --username admin --email admin@example.com 
