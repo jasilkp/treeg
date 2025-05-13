bf452aca12a8673b317ca2d2ea5c5f03d94b9083
Jasil kp <jasiljinu899@gmail.com>
2025-05-13 21:29:56 +0530

    Update build.sh to create superuser and collect static files during Railway deployment

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python manage.py migrate 
+python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py createsuperuser --noinput --username admin --email admin@example.com 
