9045166e0e28fa82442fa8c4a93d47cb5270b231
Jasil kp <jasiljinu899@gmail.com>
2025-05-13 23:39:59 +0530

    Add STATICFILES_DIRS to settings.py for correct static file collection and logo display

--- accounting/settings.py
+++ accounting/settings.py
@@ -168,6 +168,10 @@ STATIC_URL = 'static/'
 STATIC_ROOT= BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
