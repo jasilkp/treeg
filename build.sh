a69baab3834e0cce450c360f9cc7b76427045f4f
Jasil kp <jasiljinu899@gmail.com>
2025-06-14 21:54:22 +0530

    fix: Ensure dependencies are installed in build.sh before Django commands

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+pip install --upgrade pip
+pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
 # python manage.py createsuperuser --noinput --username admin --email admin@example.com 
