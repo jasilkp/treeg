5912c9edde832371c1cb69c2e990d7840df18a5b
Jasil kp <jasiljinu899@gmail.com>
2025-06-18 15:54:11 +0530

    commit all changes

--- accounts/management/commands/createsuperuser_prod.py
+++ accounts/management/commands/createsuperuser_prod.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Create a superuser for production'
+    help = 'Create or update a superuser for production'
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
@@ -10,4 +10,9 @@ class Command(BaseCommand):
             User.objects.create_superuser('admin', 'admin@example.com', '1234')
             self.stdout.write(self.style.SUCCESS('Superuser created!'))
         else:
-            self.stdout.write(self.style.WARNING('Superuser already exists.')) 
+            user = User.objects.get(username='admin')
+            user.set_password('1234')
+            user.is_superuser = True
+            user.is_staff = True
+            user.save()
+            self.stdout.write(self.style.WARNING('Superuser already existed, password reset!')) 
