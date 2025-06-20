2c8e8d7c1ada9a0e632eb21d645b33478a7269a7
Jasil kp <jasiljinu899@gmail.com>
2025-06-20 17:23:39 +0530

    commit the latest changes

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+apt-get update && apt-get install -y libpq-dev
 pip install --upgrade pip
 pip install -r requirements.txt
+python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py createsuperuser_prod
-python manage.py collectstatic --noinput
--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
 apt-get update && apt-get install -y libpq-dev
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py migrate
-python manage.py collectstatic --noinput
-python manage.py shell 
+python manage.py collectstatic --noinput 
