f52ac5517b1617f052313a315529881b6279ae1e
Jasil kp <jasiljinu899@gmail.com>
2025-06-21 11:51:15 +0530

    Commit latest changes

--- render-build.sh
+++ render-build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
+echo "--- Starting build script ---"
+echo "--- Running apt-get update and install ---"
 apt-get update && apt-get install -y libpq-dev
+echo "--- Finished apt-get ---"
+
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py migrate
